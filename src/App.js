22c47a8479c9408d9746a7fc7ef9c1c90c684e3d
Akshay27329 <akshaysharma2327@gmail.com>
2025-11-05 17:58:00 +0000

    done

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Loader from "./Loader";
+// Lazy loading components
+const Buy = lazy(() => import("./Buy"));
+const Home = lazy(() => import("./Home"));
+const Visit = lazy(() => import("./Visit"));
+const Animals = lazy(() => import("./Animals"));
+const Explore = lazy(() => import("./Explore"));
+const Purchase = lazy(() => import("./Purchase"));
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<Loader />}>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Explore" element={<Explore />} />
+          <Route path="/Visit" element={<Visit />} />
+          <Route path="/Buy" element={<Buy />} />
+          <Route path="/animals" element={<Animals />} />
+          <Route path="/Purchase" element={<Purchase />} />
+        </Routes>
+        <Footer />
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+function Fotter() {
+  return (
+    <>
+      <footer className="footer" style={{ marginTop: "10em" }}>
+       
+        <div className="ocean">
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+
+        <p>Visit our Aqarium Now , Don't Wait !</p>
+      </footer>
+    </>
+  );
+}
+
+export default Fotter;
